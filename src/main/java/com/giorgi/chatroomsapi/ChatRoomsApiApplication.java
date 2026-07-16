c84d7e1a7be6c9ea339d9475a1a6a604433ecec5
Giorgi Nikoladze <giorginikoladze963@gmail.com>
2026-07-16 17:48:49 +0400

    chore: initial project setup

---
+++ src/main/java/com/giorgi/chatroomsapi/ChatRoomsApiApplication.java
@@ -0,0 +1,13 @@
+package com.giorgi.chatroomsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRoomsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giorgi/chatroomsapi/ChatRoomsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giorgi.chatroomsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
